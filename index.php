23e6311113e2a45e073ac7faecf4cfd83481f20d
Tajree <jannatultajree5@gmail.com>
2024-02-09 19:10:14 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "My first PHP script!";
+?>
+<br>
+<?php
+$txt = "PHP";
+echo "I love $txt!";
+?>
+<br>
+<?php
+echo phpversion();
+?> 
+<br>
+<h1>My first PHP page</h1>
+<?php
+echo "Hello Junaid!";
+?>
+<br>
+<?php
+ECHO "Hello Sabrina<br>";
+echo "Hello Kaifa<br>";
+EcHo "Hello bangali<br>";
+?>
+// This is a single-line comment
+<br>
+
+# This is also a single-line comment
+<br>
+/* This is a
+multi-line comment */
+<br>
+<?php
+$txt = "W3Schools.com";
+echo "I love $txt!";
+?>
+<?php
+$x = 5;
+$y = 4;
+echo $x + $y;
+?>
+<br>
+<?php
+$x = 5;      // $x is an integer
+$y = "John"; // $y is a string
+
+echo $x;
+echo $y;
+?>
+<br>
+<pre>
+
+<?php
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+?>
+</pre>
+<br>
+<?php
+$x = "John";
+echo $x;
+?>
+<br>
+<?php
+$x = $y = $z = "Fruit";
+
+echo $x;
+echo $y;
+echo $z;
+
+?>
+<br>
+</body>
+</html>
+
+
